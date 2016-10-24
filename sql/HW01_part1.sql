25043523f5040ddddd9ca8b59e96903e3804b587
86 <leizheng8686@gmail.com>
2016-10-24 16:46:03 -0400

    hw01

---
+++ sql/HW01_part1.sql
@@ -0,0 +1,43 @@
+﻿/*
+create table sales
+	(
+		cust	varchar(20),
+		prod	varchar(20),
+		day	integer,
+		month	integer,
+		year	integer,
+		state	char(2),
+		quant	integer
+	)
+*/
+
+create view prod_max as
+(select prod, max(quant) as max_q from sales group by prod);
+
+create view prod_max_info as
+(select prod_max.prod, prod_max.max_q, sales.cust as cust_max,
+ sales.month as mon_max, sales.day as day_max, sales.year as year_max, sales.state as st_max
+from prod_max, sales where prod_max.prod = sales.prod and prod_max.max_q = sales.quant);
+
+create view prod_min as
+(select prod, min(quant) as min_q from sales group by prod);
+
+create view prod_min_info as
+(select prod_min.prod, prod_min.min_q, sales.cust as cust_min,
+ sales.month as mon_min, sales.day as day_min, sales.year as year_min, sales.state as st_min
+from prod_min, sales where prod_min.prod = sales.prod and prod_min.min_q = sales.quant);
+
+create view prod_avg as
+(select prod, round(avg(quant), 0) as avg_q from sales group by prod);
+
+
+create view part1 as
+(select prod_max_info.prod, prod_max_info.max_q, prod_max_info.cust_max, 
+prod_max_info.mon_max, prod_max_info.day_max, prod_max_info.year_max, prod_max_info.st_max,
+prod_min_info.min_q, prod_min_info.cust_min, prod_min_info.mon_min, 
+prod_min_info.day_min, prod_min_info.year_min, prod_min_info.st_min,
+prod_avg.avg_q
+from prod_max_info, prod_min_info, prod_avg
+where prod_max_info.prod = prod_min_info.prod and prod_max_info.prod = prod_avg.prod);
+
+select * from part1;
---
+++ sql/HW01_part2.sql
@@ -0,0 +1,28 @@
+﻿
+create view ct_temp as
+(select cust, prod, max(quant) as ct_max from sales 
+where state = 'CT' and year between 2000 and 2005 group by cust, prod ); 
+
+create view ct as
+(select ct_temp.*, sales.month as ct_mon, sales.day as ct_day, sales.year  as ct_yr 
+from ct_temp, sales where ct_temp.prod = sales.prod and ct_temp.cust = sales.cust 
+and ct_temp.ct_max = sales.quant and sales.year between 2000 and 2005);
+
+select * from ct;
+
+create view ny_temp as
+(select cust, prod, min(quant) as ny_min from sales where state = 'NY' group by cust, prod ); 
+
+create view ny as
+(select ny_temp.*, sales.month as ny_mon, sales.day as ny_day, sales.year as ny_yr from ny_temp, sales 
+where ny_temp.prod = sales.prod and ny_temp.cust = sales.cust and ny_temp.ny_min = sales.quant);
+
+create view nj_temp as
+(select cust, prod, min(quant) as nj_min from sales where state = 'NJ' group by cust, prod ); 
+
+create view nj as
+(select nj_temp.*, sales.month as nj_mon, sales.day as nj_day, sales.year as nj_yr from nj_temp, sales 
+where nj_temp.prod = sales.prod and nj_temp.cust = sales.cust and nj_temp.nj_min = sales.quant);
+
+select * from
+((ct natural full outer join ny) natural full outer join nj) order by cust, prod;
